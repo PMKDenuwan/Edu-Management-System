f6f264542ee0ab474a8eedd706243ba548516107
PMKDenuwan <IT22229434@my.sliit.lk>
2025-07-15 18:51:05 +0530

    Initialize the Spring boot project

---
+++ Edu-Management-System/Edu-Management-System/src/main/java/com/PMKDenuwan/Edu_Management_System/EduManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.PMKDenuwan.Edu_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManagementSystemApplication.class, args);
+	}
+
+}
